a3c58a4babf5b35d7248b3cae78d451b2ab7e202
Miguel Sanchez <mangelsr25@gmail.com>
2025-01-16 18:23:16 -0500

    [ADD] Wrapper class to combine shape, text, speed movement

--- assessment1/main.cpp
+++ assessment1/main.cpp
@@ -3,6 +3,52 @@
 #include <memory>
 #include <SFML/Graphics.hpp>
 
+class CustomShape
+{
+    float mVelocityX, mVelocityY;
+    std::shared_ptr<sf::Shape> mShape;
+    std::shared_ptr<sf::Text> mShapeLabel;
+
+public:
+    CustomShape() {}
+
+    CustomShape(float velocityX, float velocityY,
+                std::shared_ptr<sf::Shape> shape, std::shared_ptr<sf::Text> shapeLabel)
+
+        : mVelocityX(velocityX),
+          mVelocityY(velocityY),
+          mShape(shape),
+          mShapeLabel(shapeLabel)
+    {
+        sf::Vector2f shapePostiion = shape->getPosition();
+        sf::FloatRect shapeRect = mShape->getLocalBounds();
+        sf::FloatRect labelRect = mShapeLabel->getLocalBounds();
+
+        mShapeLabel->setPosition(shapePostiion.x + shapeRect.width / 2 - labelRect.width / 2,
+                                 shapePostiion.y + shapeRect.height / 2 - labelRect.height / 2);
+    }
+
+    void move()
+    {
+        sf::Vector2f currentShapePosition = mShape->getPosition();
+
+        mShape->setPosition(currentShapePosition.x + mVelocityX,
+                            currentShapePosition.y + mVelocityY);
+
+        sf::Vector2f currentShapeLabelPosition = mShapeLabel->getPosition();
+
+        mShapeLabel->setPosition(currentShapeLabelPosition.x + mVelocityX,
+                                 currentShapeLabelPosition.y + mVelocityY);
+    }
+
+    void draw(sf::RenderWindow &window)
+    {
+        move();
+        window.draw(*mShape);
+        window.draw(*mShapeLabel);
+    }
+};
+
 int main()
 {
     std::ifstream fileIn("config.txt");
@@ -14,73 +60,87 @@ int main()
     std::string fontFile;
     int fontSize, fontColorR, fontColorG, fontColorB;
 
-    std::vector<std::shared_ptr<sf::Shape>> shapes;
+    fileIn >> rowIdentifier;
 
-    while (fileIn >> rowIdentifier)
+    if (rowIdentifier == "Window")
     {
-        if (rowIdentifier == "Window")
-        {
-            fileIn >> wWidth >> wHeight;
-        }
-        else if (rowIdentifier == "Font")
-        {
-            fileIn >> fontFile >> fontSize >> fontColorR >> fontColorG >> fontColorB;
-        }
-        else if (rowIdentifier == "Circle")
-        {
-            std::string label;
-            int positionX, positionY;
-            float velocityX, velocityY;
-            int colorR, colorG, colorB;
-            int radius;
-
-            fileIn >> label;
-            fileIn >> positionX >> positionY;
-            fileIn >> velocityX >> velocityY;
-            fileIn >> colorR >> colorG >> colorB;
-            fileIn >> radius;
-
-            std::shared_ptr<sf::Shape> circle = std::make_shared<sf::CircleShape>(radius);
-            circle->setPosition(positionX, positionY);
-            circle->setFillColor(sf::Color(colorR, colorG, colorB));
-
-            shapes.push_back(circle);
-        }
-        else if (rowIdentifier == "Rectangle")
-        {
-            std::string label;
-            int positionX, positionY;
-            float velocityX, velocityY;
-            int colorR, colorG, colorB;
-            int width, height;
-
-            fileIn >> label;
-            fileIn >> positionX >> positionY;
-            fileIn >> velocityX >> velocityY;
-            fileIn >> colorR >> colorG >> colorB;
-            fileIn >> width >> height;
-
-            std::shared_ptr<sf::Shape> rect = std::make_shared<sf::RectangleShape>(sf::Vector2f(width, height));
-            rect->setPosition(positionX, positionY);
-            rect->setFillColor(sf::Color(colorR, colorG, colorB));
-
-            shapes.push_back(rect);
-        }
-        else
-        {
-            std::cout << "This row can't be parsed";
-            exit(-1);
-        }
+        fileIn >> wWidth >> wHeight;
+    }
+    else
+    {
+        std::cout << "Error reading the config file, Must start with Window config" << "\n";
+        exit(-1);
     }
 
     sf::RenderWindow window(sf::VideoMode(wWidth, wHeight), "SFML works!");
     window.setFramerateLimit(60);
+
+    fileIn >> rowIdentifier;
+
+    if (rowIdentifier == "Font")
+    {
+        fileIn >> fontFile >> fontSize >> fontColorR >> fontColorG >> fontColorB;
+    }
+    else
+    {
+        std::cout << "Error reading the config file, Second line should be Font config" << "\n";
+        exit(-1);
+    }
 
     sf::Font myFont;
     if (!myFont.loadFromFile(fontFile))
     {
         std::cerr << "Could not load font!";
         exit(-1);
+    }
+
+    std::vector<CustomShape> shapes;
+
+    while (fileIn >> rowIdentifier)
+    {
+        if (rowIdentifier == "Circle" || rowIdentifier == "Rectangle")
+        {
+            std::string label;
+            int positionX, positionY;
+            float velocityX, velocityY;
+            int colorR, colorG, colorB;
+
+            fileIn >> label;
+            fileIn >> positionX >> positionY;
+            fileIn >> velocityX >> velocityY;
+            fileIn >> colorR >> colorG >> colorB;
+
+            std::shared_ptr<sf::Text> text = std::make_shared<sf::Text>(label, myFont, fontSize);
+            text->setFillColor(sf::Color(fontColorR, fontColorG, fontColorB));
+
+            if (rowIdentifier == "Circle")
+            {
+                int radius;
+                fileIn >> radius;
+
+                std::shared_ptr<sf::Shape> circle = std::make_shared<sf::CircleShape>(radius);
+                circle->setPosition(positionX, positionY);
+                circle->setFillColor(sf::Color(colorR, colorG, colorB));
+
+                shapes.push_back(CustomShape(velocityX, velocityY, circle, text));
+            }
+            else if (rowIdentifier == "Rectangle")
+            {
+                int width, height;
+                fileIn >> width >> height;
+
+                std::shared_ptr<sf::Shape> rect = std::make_shared<sf::RectangleShape>(sf::Vector2f(width, height));
+                rect->setPosition(positionX, positionY);
+                rect->setFillColor(sf::Color(colorR, colorG, colorB));
+
+                shapes.push_back(CustomShape(velocityX, velocityY, rect, text));
+            }
+        }
+        else
+        {
+            std::cout << "This row can't be parsed";
+            exit(-1);
+        }
     }
 
     while (window.isOpen())
@@ -97,7 +157,7 @@ int main()
 
         for (auto &shape : shapes)
         {
-            window.draw(*shape);
+            shape.draw(window);
         }
 
         window.display();
